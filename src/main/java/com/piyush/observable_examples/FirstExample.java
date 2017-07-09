6781ffb17e3e4141d6f22e996d528b8bdb08d593
PiyushNagar <piyush.nagar@evivehealth.com>
2017-07-09 19:09:10 +0530

    Flowable

--- src/main/java/com/piyush/observable_examples/FirstExample.java
+++
@@ -1,7 +0,0 @@
-package com.piyush.observable_examples;
-
-/**
- * @author evivehealth on 5/6/17.
- */
-public class FirstExample {
-}
---
+++ src/main/java/com/piyush/observable_examples/ParallelFlowableExample.java
@@ -0,0 +1,23 @@
+package com.piyush.observable_examples;
+
+import com.google.common.collect.ImmutableList;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+import java.util.logging.Logger;
+
+/**
+ * @author Piyush Nagar on 5/6/17.
+ */
+class FirstExample {
+  private FirstExample() {
+  }
+  public static void main(String[] args) {
+    Logger logger = Logger.getLogger("FirstExample");
+    Flowable.fromIterable(ImmutableList.of("abc", "def"))
+        .parallel()
+        .runOn(Schedulers.computation())
+        .doOnNext(x -> logger.info("Thread is" + Thread.currentThread().getName()))
+        .sequential()
+        .subscribe();
+  }
+}
